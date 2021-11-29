a481f65c0bfaa7aac1433aa45544ad254a3f635e
TheBlackIgor <swierczynskigor@gmail.com>
2021-11-29 19:21:22 +0100

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const fs = require('fs')
 
 var bgImages = fs.readdirSync('./static/gfx/main-bg/');
